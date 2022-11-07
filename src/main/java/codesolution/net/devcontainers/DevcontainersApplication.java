0a49721ba4d7f7b1b45643ea621cf3544b6420ba
r3ts3l <llopez9047@gmail.com>
2022-11-07 03:58:22 -0500

    Start containers from main enabled by environment variable

--- src/main/java/codesolution/net/devcontainers/DevcontainersApplication.java
+++ src/main/java/codesolution/net/devcontainers/DevcontainersApplication.java
@@ -1,5 +1,6 @@
 package codesolution.net.devcontainers;
 
+import codesolution.net.devcontainers.model.DevcontainerInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevcontainersApplication {
 
     public static void main(String[] args) {
+        if (System.getenv("ENV").equals("local")) {
+            DevcontainerInitializer.getProperties().forEach((k, v) -> System.setProperty(k, String.valueOf(v)));
+        }
         SpringApplication.run(DevcontainersApplication.class, args);
     }
 }
